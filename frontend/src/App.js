7462aa55594eb67d2a5a179c9466eb66456f221c
G-Andrey <42978646+G-Andrey@users.noreply.github.com>
2021-05-10 22:06:16 -0400

    upload pictures

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+
 import './App.css';
 
 function App() {
+
+  const [image, setImage] = useState({ preview: "", raw: "" });
+  const [predictedImage, setPredictedImage] = useState({url: ""});
+  const [loading, setLoading] = useState(false)
+
+  const handleImageChange = e => {
+    if (e.target.files.length) {
+      setImage({
+        preview: URL.createObjectURL(e.target.files[0]),
+        raw: e.target.files[0]
+      });
+    }
+  };
+
+  const handlePrediction = e => {
+    e.preventDefault();
+
+    setPredictedImage(
+      {url: URL.createObjectURL(image.raw)}
+    ) 
+
+    //MAKE FLASK POST REQ HERE??
+  }
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Title">
+        Test Our Model
+      </div>
+      <div className="Input-box">
+        <input className="fileInput" 
+          type="file" 
+          onChange={handleImageChange} 
+        />
+        <button className="submitButton" 
+          type="submit" 
+          onClick={handlePrediction}>
+            Run Prediction
+        </button>
+      </div>
+      <div className="original-text">
+        Original
+      </div>
+      {image.preview ? (
+        <img src={image.preview} alt="dummy" width="512" height="256" />
+        ) : (
+        <>
+          <h5 className="text-center">Upload your photo</h5>
+        </>
+      )}
+      <div className="original-text">
+        Predicted
+      </div>
+      
+      {predictedImage.url ? (
+        <img src={predictedImage.url} alt="dummy" width="512" height="256" />
+        ) : (
+        <>
+          <h5 className="text-center">Waiting for prediction</h5>
+        </>
+      )}
     </div>
   );
 }
