c02f4f617e0fa75e5bd1ffa82a79e3fec8dcdbd0
G-Andrey <42978646+G-Andrey@users.noreply.github.com>
2021-05-11 12:38:45 -0400

    dropdown

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,8 @@ function App() {
 
   const [image, setImage] = useState({ preview: "", raw: "" });
   const [predictedImage, setPredictedImage] = useState({url: ""});
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
+  const [modelType, setModelType] = useState("model_fpn");
 
   const handleImageChange = e => {
     if (e.target.files.length) {
@@ -18,6 +19,10 @@ function App() {
     }
   };
 
+  const handleDropdownChange = e => {
+    setModelType(e.target.value)
+  }
+
   const handlePrediction = e => {
     e.preventDefault();
     
@@ -26,6 +31,18 @@ function App() {
     form_data.append('image', image.raw);
 
     let url = 'http://localhost:5000/predict';
+
+    if(modelType == "model_fpn_multi")
+      url = url + "_multi"
+
+    else if(modelType == "model_unet")
+      url = url + "_unet"
+
+    else if(modelType == "model_fpn")
+      url = url + "_fpn"
+
+    else
+      url = url + "_linknet"
 
     axios.post(url, form_data, {
       headers: {
@@ -39,11 +56,9 @@ function App() {
           
         })
         .catch(err => console.log(err))
-    console.log(predictedImage)
   }
 
   return (
-
     <div className="App">
       <div className="Title">
         Test Our Model
@@ -58,6 +73,13 @@ function App() {
           onClick={handlePrediction}>
             Run Prediction
         </button>
+        <label for="model_type" className="model-type-label">Model Type:</label>
+        <select name="model_type" onChange={handleDropdownChange}>
+          <option value="model_fpn">FPN</option>
+          <option value="model_unet">UNET</option>
+          <option value="model_linknet">LINKET</option>
+          <option value="model_fpn_multi">FPN Multiclass</option>
+        </select>
       </div>
       <div className="original-text">
         Original
