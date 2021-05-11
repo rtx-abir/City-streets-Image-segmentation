f54b0f850a65ba91fda7d42c2732ab2525788f83
G-Andrey <42978646+G-Andrey@users.noreply.github.com>
2021-05-11 16:41:32 -0400

    styling + unet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [modelType, setModelType] = useState("model_fpn");
   const [errMesg, setErrMesg] = useState("");
+  const sidebarRef = React.useRef(null)
 
   const handleImageChange = e => {
     if (e.target.files.length) {
@@ -20,13 +21,17 @@ function App() {
     }
   };
 
+  const handleSideBarClick = e => {
+    sidebarRef.current.click()
+  }
+
   const handleDropdownChange = e => {
     setModelType(e.target.value)
   }
 
   const handlePrediction = e => {
     e.preventDefault();
-
+    handleSideBarClick()
     setErrMesg("");
     setLoading(true); 
 
@@ -69,77 +74,128 @@ function App() {
 
   return (
     <div className="App">
-      <input type="checkbox" class="openSidebarMenu" id="openSidebarMenu"/>
-        <label for="openSidebarMenu" class="sidebarIconToggle">
-          <div class="spinner diagonal part-1"></div>
-          <div class="spinner horizontal"></div>
-          <div class="spinner diagonal part-2"></div>
-        </label>
-        <div id="sidebarMenu">
-          <ul class="sidebarMenuInner">
-            <li>XYZ<span>test</span></li>
-            <li><a href="https://vanila.io" target="_blank">Company</a></li>
-            <li><a href="https://instagram.com/plavookac" target="_blank">Instagram</a></li>
-          </ul>
-        </div>
-      <div className="Input-box">
-        <input className="fileInput" 
-          type="file" 
-          onChange={handleImageChange} 
-        />
-        <button className="submitButton" 
-          type="submit" 
-          onClick={handlePrediction}>
-            Run Prediction
+        <input type="checkbox" class="openSidebarMenu" id="openSidebarMenu" onClick={handleSideBarClick}/>
+          <label for="openSidebarMenu" class="sidebarIconToggle" ref={sidebarRef}>
+            <div class="spinner diagonal part-1"></div>
+            <div class="spinner horizontal"></div>
+            <div class="spinner diagonal part-2"></div>
+          </label>
+            <div id="sidebarMenu">
+              <ul class="sidebarMenuInner">
+                <li>
+                  <input className="fileInput" 
+                    type="file" 
+                    onChange={handleImageChange} 
+                  />
+              </li>
+                <li>
+                  <div className="model-type-text">
+                    Model Type
+                  </div>
+                  <select name="model_type" onChange={handleDropdownChange} className="model-type-drpdwn">
+                    <option value="model_fpn">FPN Binary</option>
+                    <option value="model_unet">UNET Binary</option>
+                    <option value="model_linknet">LINKET Binary</option>
+                    <option value="model_fpn_multi">FPN Multiclass</option>
+                  </select>
+                </li>
+                <li>
+                  <button className="submit-btn" 
+                    type="submit" 
+                    onClick={handlePrediction}>
+                      Run Prediction
+                  </button>
+                </li>
+              </ul>
+            </div>
+            {image.preview? 
+            (null) 
+            : 
+            (<div className="Title">
+              Welcome To CityScapes Segmentation 
+            </div>)
+            }
+      <div className="Middle-bar" style={image.preview ? {width:'900px', margin:'auto', background: 'rgba(252, 252, 252, 0.5)', height: '100vh'}: null}>
+        <button onClick={handleSideBarClick} className="start-pred-button">
+          <div className="start-pred-text">
+            Start Prediction
+          </div>
         </button>
-        <label for="model_type" className="model-type-label">Model Type:</label>
-        <select name="model_type" onChange={handleDropdownChange}>
-          <option value="model_fpn">FPN Binary</option>
-          <option value="model_unet">UNET Binary</option>
-          <option value="model_linknet">LINKET Binary</option>
-          <option value="model_fpn_multi">FPN Multiclass</option>
-        </select>
+        {loading ? (
+           <div className="Loading-Mesg">
+             Processing
+           </div>
+           ) : (
+           <>
+           </>
+        )}
+        {errMesg ? (
+           <div className="Err-Mesg">
+             {errMesg}
+           </div>
+           ) : (
+           <>
+           </>
+        )}
+        {image.preview ? (
+          <div className="preview-img">
+            <img src={image.preview} alt="dummy" width="800" height="400" />
+          </div>
+          ) : (
+          <>
+          </>
+        )}
+        {predictedImage.url ? (
+          <div className="predicted-trim">
+            <img src={URL.createObjectURL(predictedImage.url)} alt="dummy" className="pred-img"/>
+          </div>
+          ) : (
+          <>
+          </>
+        )}
       </div>
-      {errMesg ? (
-        <div className="Err-Mesg">
-          {errMesg}
-        </div>
-        ) : (
-        <>
-        </>
-      )}
-      {loading ? (
-        <div className="Loading-Mesg">
-          Processing
-        </div>
-        ) : (
-        <>
-        </>
-      )}
-      <div className="original-text">
-        Original
-      </div>
-      {image.preview ? (
-        <img src={image.preview} alt="dummy" width="800" height="400" />
-        ) : (
-        <>
-          <h5 className="text-center">Upload your photo</h5>
-        </>
-      )}
-      <div className="original-text">
-        Predicted
-      </div>
-      {predictedImage.url ? (
-        <div className="predicted-trim">
-          <img src={URL.createObjectURL(predictedImage.url)} alt="dummy" className="pred-img"/>
-        </div>
-        ) : (
-        <>
-          <h5 className="text-center">Waiting for prediction</h5>
-        </>
-      )}
     </div>
   );
 }
 
 export default App;
+
+
+// {errMesg ? (
+//   <div className="Err-Mesg">
+//     {errMesg}
+//   </div>
+//   ) : (
+//   <>
+//   </>
+// )}
+// {loading ? (
+//   <div className="Loading-Mesg">
+//     Processing
+//   </div>
+//   ) : (
+//   <>
+//   </>
+// )}
+// <div className="original-text">
+//   Original
+// </div>
+// {image.preview ? (
+//   <img src={image.preview} alt="dummy" width="800" height="400" />
+//   ) : (
+//   <>
+//     <h5 className="text-center">Upload your photo</h5>
+//   </>
+// )}
+// <div className="original-text">
+//   Predicted
+// </div>
+// {predictedImage.url ? (
+//   <div className="predicted-trim">
+//     <img src={URL.createObjectURL(predictedImage.url)} alt="dummy" className="pred-img"/>
+//   </div>
+//   ) : (
+//   <>
+//     <h5 className="text-center">Waiting for prediction</h5>
+//   </>
+// )}
